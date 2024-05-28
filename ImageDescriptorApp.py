285674151971af82eba3eb362d157398d7d9d0ca
jitjat123 <83438039+jitjat123@users.noreply.github.com>
2024-05-28 11:36:00 -0500

    Update ImageDescriptorApp.py

--- ImageDescriptorApp.py
+++ ImageDescriptorApp.py
@@ -10,8 +10,12 @@ from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration, DetrImageProcessor, DetrForObjectDetection
 import torch
 
-processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+try:
+    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+except ImportError as e:
+    st.error(f"Error importing a module: {e}")
+    st.stop()
 
 
 
